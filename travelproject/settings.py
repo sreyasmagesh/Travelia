6c8c9f8ac615103704a5155ffedec17b4e4a27f0
sreyas <sreyaskakkodi@gmail.com>
2022-06-13 14:00:15 -0700

    chang

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 ALLOWED_HOSTS=['*']
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 # Default primary key field type
