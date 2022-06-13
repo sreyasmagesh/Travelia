b4ab3eb04f32c0658d7cf0ac371ecdca4b7bbfab
sreyas <sreyaskakkodi@gmail.com>
2022-06-13 12:53:59 -0700

    changeded

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
+STATICFILES_DIRS=(os.path.join(PROJECT_ROOT,'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
